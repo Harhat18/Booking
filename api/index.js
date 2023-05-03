caf543a2c53bfd7c82ae139ceb859707b1551287
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2023-05-03 20:13:42 +0300

    add axios

--- api/index.js
+++ api/index.js
@@ -2,28 +2,33 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
-import usersRoute from "./routes/users.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 const app = express();
 dotenv.config();
+
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
-    console.log("connected to mongoDB.");
+    console.log("Connected to mongoDB.");
   } catch (error) {
     throw error;
   }
 };
 
-mongoose.connection.on("disconnect", () => {
-  console.log("mongoDB disconnect.");
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
 });
 
 //middlewares
-app.use(cookieParser);
+app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
@@ -31,7 +36,7 @@ app.use("/api/rooms", roomsRoute);
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
-  const errorMessage = err.message || "Someting went wrong";
+  const errorMessage = err.message || "Something went wrong!";
   return res.status(errorStatus).json({
     success: false,
     status: errorStatus,
@@ -42,5 +47,5 @@ app.use((err, req, res, next) => {
 
 app.listen(4000, () => {
   connect();
-  console.log("connected to backend.");
+  console.log("Connected to backend.");
 });
